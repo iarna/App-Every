7a544db7e3770fc02dbe2e81f7778001bca21463
Rebecca Turner <turner@mikomi.org>
2012-07-15 22:48:37 -0400

    Manually build the dependency list

--- build_fatpack.sh
+++ build_fatpack.sh
@@ -4,7 +4,9 @@ OUTSCRIPT=packed/every
 
 [ ! -d packed ] && mkdir packed
 
-fatpack tree $(fatpack packlists-for $(fatpack trace --to=- $INSCRIPT)) && \
+DEPENDENCIES="App/Every.pm Digest/MD5.pm Digest/base.pm Digest/Perl/MD5.pm"
+
+fatpack tree $(fatpack packlists-for $DEPENDENCIES) && \
 fatpack file > fatlib.pl 2>/dev/null && \
 perl -pe 's{#\s*__FATPACK__\s*}{qx( cat fatlib.pl )}ge; $_' $INSCRIPT > $OUTSCRIPT &&
 chmod a+x $OUTSCRIPT
